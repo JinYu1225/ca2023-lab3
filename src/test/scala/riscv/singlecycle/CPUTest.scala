49416c215aebc96eb6e3e9ab8d3e6b3dec670227
JinYu1225 <zcxvedenlin@gmail.com>
2024-01-15 15:00:53 +0800

    Modify makefile and HW2.S file

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -63,6 +63,22 @@ class TestTopModule(exeFilename: String) extends Module {
   io.mem_debug_read_data    := mem.io.debug_read_data
 }
 
+class HammingDistanceTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior.of("Single Cycle CPU")
+  it should "cal hamming distance of 0x100000 and 0xFFFFF" in {
+    test(new TestTopModule("HammingDistance.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+      for (i <- 1 to 50) {
+        c.clock.step(1000)
+        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+      }
+
+	    c.io.regs_debug_read_address.poke(10.U) //a0
+      c.clock.step()
+      c.io.regs_debug_read_data.expect(21.U)
+    }
+  }
+}
+
 class FibonacciTest extends AnyFlatSpec with ChiselScalatestTester {
   behavior.of("Single Cycle CPU")
   it should "recursively calculate Fibonacci(10)" in {
